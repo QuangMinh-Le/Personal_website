d96743efa7b6093fe5f04ded3c59fc4384358706
quangminh <quangminh.ottawa@gmail.com>
2023-08-27 17:55:48 -0400

    Done responsive navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,11 +16,11 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Adrian</span>
+            Hi, I'm <span className='text-[#915EFF]'>Minh</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
+            I am 3rd-year CS student <br className='sm:block hidden' />
+            at University of Ottawa
           </p>
         </div>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
   const [active, setActive] = useState('');
+  const [toggle, setToggle] = useState(false);
   return (
     <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
         <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
@@ -18,8 +19,53 @@ const Navbar = () => {
                 }}
           >
             <img src={logo} alt="logo" className="w-9 h-9 object-ontain"/>
-            <p className="text-white text-[18px] font-bold cursor-pointer"><span className="sm:block hidden">Quang Minh Le | Full-stack Developer</span></p>
+            <p className="text-white text-[18px] font-bold cursor-pointer"><span className="sm:block hidden">Quang Minh Le | Tech Lover</span></p>
           </Link>
+          <ul className="list-none hidden sm:flex flex-row gap-10">
+             {navLinks.map((Link) => (
+              <li
+                key={Link.id}
+                className={`${
+                  active === Link.title
+                    ? "text-white"
+                    : "text-secondary"
+                } hover:text-white text-[18px] font-medium cursor-pointer`}
+                onClick={() => setActive(Link.title)}
+              >
+                <a href={`#${Link.id}`}>{Link.title}</a>
+              </li>
+             ))}
+          </ul>
+          <div className="sm:hidden flex flex-1 justify-end items-center">
+              <img 
+                src={toggle ? close : menu}
+                alt="menu"
+                className="w-[28px] h-[28px] object-contain cursor-pointer"
+                onClick={() => setToggle(!toggle)}
+              />
+              
+              <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 min-w-[140px] z-10 rounded-xl`}>
+                <ul className="list-none flex justify-end items-start flex-col gap-4">
+                  {navLinks.map((Link) => (
+                    <li
+                      key={Link.id}
+                      className={`${
+                        active === Link.title
+                          ? "text-white"
+                          : "text-secondary"
+                      } font-poppins font-medium cursor-pointer text-[16px]`}
+                      onClick={() => {
+                        setToggle(!toggle);
+                        setActive(Link.title)
+                        }}
+                    >
+                      <a href={`#${Link.id}`}>{Link.title}</a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+
+          </div>
         </div>
     </nav>
   );
